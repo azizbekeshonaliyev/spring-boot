9e6ff5ce1a44e717c8b2a923d18cc998edf94763
azizbekeshonaliyev <1996azizbekeshonaliyev@gmail.com>
2021-07-08 22:18:40 +0500

    initial commit

---
+++ src/main/java/com/example/tom/Category/Category.java
@@ -0,0 +1,10 @@
+package com.example.tom.Category;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Category {
+
+
+
+}
---
+++ src/main/java/com/example/tom/Product/Product.java
@@ -0,0 +1,4 @@
+package com.example.tom.Product;
+
+public class Product {
+}
